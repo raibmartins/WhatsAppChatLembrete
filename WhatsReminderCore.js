2104bb95c705f64ef7891c2d76f9cf1bcc9d4434
Raí Bonassa Martins <raibmartins@hotmail.com>
2025-12-15 12:52:55 -0300

    Scripts para uso no tempermonkey

---
+++ WhatsReminderCore.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         WhatsApp Chat Lembrete
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Injeta botão e janela no WhatsApp Web
+// @match        https://web.whatsapp.com/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/raibmartins/WhatsAppChatLembrete/main/botao.js
+// @require      https://raw.githubusercontent.com/raibmartins/WhatsAppChatLembrete/main/janela.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log('Scripts botao.js e janela.js carregados');
+})();
---
+++ WhatsReminderScript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         WhatsApp Chat Lembrete
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Injeta botão e janela no WhatsApp Web
+// @match        https://web.whatsapp.com/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/raibmartins/WhatsAppChatLembrete/main/WhatsReminderCore.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log('Scripts botao.js e janela.js carregados');
+})();
